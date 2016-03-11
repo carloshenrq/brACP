d1b71a2645ac812df2bec5cb11cf9b599fd44828
Carlos Henrique <carloshlfzanon@gmail.com>
2016-03-11 16:42:28 -0300

    Adicionado primeiros campos do configurador de instalação.
    issue #5- Mais alguns passos...

--- install.php
+++ install.php
@@ -26,6 +26,31 @@ if(file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'config.php'))
 // Obtém as permissões de arquivo para notificar o usuário sobre as informações.
 $writeable = is_writable(__DIR__);
 
+// Configurações padrão.
+$config = [
+    'BRACP_DEFAULT_TIMEZONE'                => @date_default_timezone_get(),
+    'BRACP_URL'                             => 'http://'.$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+    'BRACP_DIR_INSTALL_URL'                 => $_SERVER['REQUEST_URI'],
+    'BRACP_TEMPLATE_DIR'                    => __DIR__ . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR,
+    'BRACP_ENTITY_DIR'                      => __DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'Model' . DIRECTORY_SEPARATOR . 'Entity',
+    'BRACP_MD5_PASSWORD_HASH'               => 1,
+    'BRACP_MAIL_REGISTER_ONCE'              => 1,
+    'BRACP_MAIL_SHOW_LOG'                   => 1,
+    'BRACP_CHANGE_MAIL_DELAY'               => 60,
+    'BRACP_ALLOW_CHANGE_MAIL'               => 1,
+    'BRACP_ALLOW_CREATE_ACCOUNT'            => 1,
+    'BRACP_ALLOW_RECOVER'                   => 1,
+    'BRACP_ALLOW_ADMIN'                     => 1,
+    'BRACP_ALLOW_ADMIN_GMLEVEL'             => 99,
+    'BRACP_ALLOW_LOGIN_GMLEVEL'             => 0,
+    'BRACP_ALLOW_ADMIN_CHANGE_PASSWORD'     => 0,
+    'BRACP_ALLOW_RANKING'                   => 1,
+    'BRACP_ALLOW_SHOW_CHAR_STATUS'          => 1,
+    'BRACP_ALLOW_RANKING_ZENY'              => 1,
+    'BRACP_ALLOW_RANKING_ZENY_SHOW_ZENY'    => 1,
+    'BRACP_DEVELOP_MODE'                    => 0,
+];
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -34,6 +59,35 @@ $writeable = is_writable(__DIR__);
 
         <link rel="stylesheet" type="text/css" href="css/install.css"/>
         <link rel="stylesheet" type="text/css" href="css/button.css"/>
+
+        <script src="js/jquery-2.1.4.js"></script>
+        <script>
+            var defaultConfig = <?php echo json_encode($config); ?>;
+
+            function defaultConfigLoad()
+            {
+                $.each(defaultConfig, function(index, value) {
+                    $('#' + index).val(value);
+                });
+            }
+
+            +function($)
+            {
+                $(document).on('click', 'input[type="reset"]', function(e) {
+                    e.preventDefault();
+
+                    $(this).closest('form').each(function() {
+                        this.reset();
+                    });
+
+                    defaultConfigLoad();
+                });
+
+                $(document).ready(function() {
+                    defaultConfigLoad();
+                });
+            } (window.jQuery);
+        </script>
     </head>
     <body>
         <div class="bracp-install-body">
@@ -60,8 +114,8 @@ $writeable = is_writable(__DIR__);
 
                 <ul class="bracp-install-tabs">
                     <li><label for="conf-general" class="btn">Configurações Gerais</label></li>
-                    <li><label for="conf-mysql" class="btn">Banco de Dados</label></li>
-                    <li><label for="conf-mail" class="btn">MySQL</label></li>
+                    <li><label for="conf-mysql" class="btn">MySQL</label></li>
+                    <li><label for="conf-mail" class="btn">Servidor de E-mail</label></li>
                     <li><label for="conf-captcha" class="btn">reCAPTCHA</label></li>
                     <li><label for="conf-donation" class="btn">PagSeguro</label></li>
                     <li><label for="conf-others" class="btn">Outros</label></li>
@@ -71,6 +125,146 @@ $writeable = is_writable(__DIR__);
                     <input name="conf-tab" id="conf-general" class="bracp-install-tab-radio" type="radio" checked/>
                     <div class="bracp-install-tab-div">
                         <h1>Configurações Gerais</h1>
+
+                        <br>
+                        <div class="bracp-install-label-data">
+                            <label>
+                                Fuso Horário:<br>
+                                <select id="BRACP_DEFAULT_TIMEZONE" name="BRACP_DEFAULT_TIMEZONE">
+                                    <?php foreach(timezone_identifiers_list() as $timeZone) { ?>
+                                        <option value="<?php echo $timeZone; ?>"><?php echo $timeZone; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </label>
+
+                            <label>
+                                URL de Acesso:<br>
+                                <input id="BRACP_URL" name="BRACP_URL" type="text" value="" size="50"/>
+                            </label>
+                            <label>
+                                URL de Instalação:<br>
+                                <input id="BRACP_DIR_INSTALL_URL" name="BRACP_DIR_INSTALL_URL" type="text" value="" size="20"/>
+                            </label>
+                        </div>
+                        <br>
+                        <div class="bracp-install-label-data">
+                            <label>
+                                Caminho arquivos de template:<br>
+                                <input id="BRACP_TEMPLATE_DIR" name="BRACP_TEMPLATE_DIR" type="text" value="" size="118"/>
+                            </label>
+                        </div>
+                        <div class="bracp-install-label-data">
+                            <label>
+                                Caminho arquivos de entidades:<br>
+                                <input id="BRACP_ENTITY_DIR" name="BRACP_ENTITY_DIR" type="text" value="" size="118"/>
+                            </label>
+                        </div>
+                        <br>
+                        <br>
+                        <div class="bracp-install-label-data">
+                            <label>
+                                MD5 em Senhas:<br>
+                                <select id="BRACP_MD5_PASSWORD_HASH" name="BRACP_MD5_PASSWORD_HASH">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                            <label>
+                                Permitir alterar e-mail:<br>
+                                <select id="BRACP_ALLOW_CHANGE_MAIL" name="BRACP_ALLOW_CHANGE_MAIL">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                            <label>
+                                Exibir alterações de e-mail:<br>
+                                <select id="BRACP_MAIL_SHOW_LOG" name="BRACP_MAIL_SHOW_LOG">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                            <label>
+                                Delay para mudar e-mail (minutos):<br>
+                                <input id="BRACP_CHANGE_MAIL_DELAY" name="BRACP_CHANGE_MAIL_DELAY" type="text" value="" size="3"/>
+                            </label>
+                        </div>
+                        <div class="bracp-install-label-data">
+                            <label>
+                                Permitir novas contas:<br>
+                                <select id="BRACP_ALLOW_CREATE_ACCOUNT" name="BRACP_ALLOW_CREATE_ACCOUNT">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                            <label>
+                                Bloquear novas contas com e-mails duplicado:<br>
+                                <select id="BRACP_MAIL_REGISTER_ONCE" name="BRACP_MAIL_REGISTER_ONCE">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                            <label>
+                                Modo administrador:<br>
+                                <select id="BRACP_ALLOW_ADMIN" name="BRACP_ALLOW_ADMIN">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                            <label>
+                                Nivel administrador:<br>
+                                <input id="BRACP_ALLOW_ADMIN_GMLEVEL" name="BRACP_ALLOW_ADMIN_GMLEVEL" type="text" value="" size="3"/>
+                            </label>
+                        </div>
+                        <div class="bracp-install-label-data">
+                            <label>
+                                Nivel para login:<br>
+                                <input id="BRACP_ALLOW_LOGIN_GMLEVEL" name="BRACP_ALLOW_LOGIN_GMLEVEL" type="text" value="" size="3"/>
+                            </label>
+                            <label>
+                                Permitir administrar trocar senha:<br>
+                                <select id="BRACP_ALLOW_ADMIN_CHANGE_PASSWORD" name="BRACP_ALLOW_ADMIN_CHANGE_PASSWORD">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                            <label>
+                                Exibir rankings:<br>
+                                <select id="BRACP_ALLOW_RANKING" name="BRACP_ALLOW_RANKING">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                            <label>
+                                Exibir online/offline nos rankings:<br>
+                                <select id="BRACP_ALLOW_SHOW_CHAR_STATUS" name="BRACP_ALLOW_SHOW_CHAR_STATUS">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div class="bracp-install-label-data">
+                            <label>
+                                Permite ranking zeny:<br>
+                                <select id="BRACP_ALLOW_RANKING_ZENY" name="BRACP_ALLOW_RANKING_ZENY">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                            <label>
+                                Exibir zenys no ranking:<br>
+                                <select id="BRACP_ALLOW_RANKING_ZENY_SHOW_ZENY" name="BRACP_ALLOW_RANKING_ZENY_SHOW_ZENY">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                            <label>
+                                Modo desenvolvedor:<br>
+                                <select id="BRACP_DEVELOP_MODE" name="BRACP_DEVELOP_MODE">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                        </div>
                     </div>
 
                     <input name="conf-tab" id="conf-mysql" class="bracp-install-tab-radio" type="radio"/>
@@ -81,6 +275,15 @@ $writeable = is_writable(__DIR__);
                     <input name="conf-tab" id="conf-mail" class="bracp-install-tab-radio" type="radio"/>
                     <div class="bracp-install-tab-div">
                         <h1>Servidor de E-mail</h1>
+                        <div class="bracp-install-label-data">
+                            <label>
+                                Permitir recuperar contas:<br>
+                                <select id="BRACP_ALLOW_RECOVER" name="BRACP_ALLOW_RECOVER">
+                                    <option value="0">Não</option>
+                                    <option value="1">Sim</option>
+                                </select>
+                            </label>
+                        </div>
                     </div>
 
                     <input name="conf-tab" id="conf-captcha" class="bracp-install-tab-radio" type="radio"/>
