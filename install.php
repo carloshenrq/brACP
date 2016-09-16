ec90ac978b48d5c927defada4222610689e598f4
Carlos Henrique <carloshlfzanon@gmail.com>
2016-09-16 10:50:39 -0300

    Adicionado configurações de 'REGEXP'

--- install.php
+++ install.php
@@ -49,6 +49,25 @@ $PRE_REQUISITES = array(
     'zip_ext'           => extension_loaded('zip'),
 );
 
+// Teste para as variaveis de configuração do painel de controle.
+$_CONFIG_DATA = array(
+    // Endereços (STEP=3)
+    'BRACP_URL'                         =>  'http' . ((isset($_SERVER['HTTPS'])) ? 's':'') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+    'BRACP_DIR_INSTALL_URL'             =>  $_SERVER['REQUEST_URI'],
+    'BRACP_TEMPLATE_DIR'                =>  dirname(__FILE__) . DIRECTORY_SEPARATOR . 'templates',
+    'BRACP_ENTITY_DIR'                  =>  dirname(__FILE__) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'Model',
+    'BRACP_DEFAULT_TIMEZONE'            =>  @date_default_timezone_get(),
+
+    // Expressões regulares (STEP=4)
+    'BRACP_REGEXP_EMAIL'                => '[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}',
+
+    // Recaptcha
+    'BRACP_RECAPTCHA_ENABLED'           => false,
+    'BRACP_RECAPTCHA_PRIVATE_KEY'       => '',
+    'BRACP_RECAPTCHA_PUBLIC_KEY'        => '',
+    'BRACP_RECAPTCHA_PRIVATE_URL'       => 'https://www.google.com/recaptcha/api/siteverify',
+);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -72,13 +91,20 @@ $PRE_REQUISITES = array(
 
             install.controller('install', ['$scope', '$http', function($scope, $http) {
 
-                $scope.STEP = 1;
+                $scope.STEP = 5;
                 $scope.STEP_ERROR   = [];
                 $scope.STEP_WARNING = [];
                 $scope.STEP_SUCCESS = [];
 
                 $scope.PRE_REQUISITES = <?php echo json_encode($PRE_REQUISITES); ?>;
                 $scope.ACCEPT_TERMS = false;
+
+                // Variaveis de configuração inicial.
+                $scope.INSTALL_VARS = <?php echo json_encode($_CONFIG_DATA); ?>;
+
+                // Configuração para expressão regular de usuários.
+                $scope.REGEXP_USERNAME = '10';
+                $scope.REGEXP_PASSWORD = '02';
 
                 // Testa os requisitos do step 1
                 window.jQuery.each($scope.PRE_REQUISITES, function(index, value) {
@@ -191,67 +217,67 @@ $PRE_REQUISITES = array(
                             <div class="row">
                                 <div class="cell">Extensão cURL:</div>
                                 <div class="cell" ng-class="(PRE_REQUISITES.curl_ext ? 'cell-success' : 'cell-error')">
-                                    {{(PRE_REQUISITES.curl_ext ? 'OK':'Verifique as extensões carregadas.')}}
+                                    {{(PRE_REQUISITES.curl_ext ? 'OK':'Por favor, habilite a extensão "cURL".')}}
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="cell">Extensão Hash:</div>
                                 <div class="cell" ng-class="(PRE_REQUISITES.hash_ext ? 'cell-success' : 'cell-error')">
-                                    {{(PRE_REQUISITES.hash_ext ? 'OK':'Verifique as extensões carregadas.')}}
+                                    {{(PRE_REQUISITES.hash_ext ? 'OK':'Por favor, habilite a extensão "hash".')}}
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="cell">Extensão Json:</div>
                                 <div class="cell" ng-class="(PRE_REQUISITES.json_ext ? 'cell-success' : 'cell-error')">
-                                    {{(PRE_REQUISITES.json_ext ? 'OK':'Verifique as extensões carregadas.')}}
+                                    {{(PRE_REQUISITES.json_ext ? 'OK':'Por favor, habilite a extensão "json".')}}
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="cell">Extensão Xml:</div>
                                 <div class="cell" ng-class="(PRE_REQUISITES.xml_ext ? 'cell-success' : 'cell-error')">
-                                    {{(PRE_REQUISITES.xml_ext ? 'OK':'Verifique as extensões carregadas.')}}
+                                    {{(PRE_REQUISITES.xml_ext ? 'OK':'Por favor, habilite a extensão "xml".')}}
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="cell">Extensão LibXml:</div>
                                 <div class="cell" ng-class="(PRE_REQUISITES.libxml_ext ? 'cell-success' : 'cell-error')">
-                                    {{(PRE_REQUISITES.libxml_ext ? 'OK':'Verifique as extensões carregadas.')}}
+                                    {{(PRE_REQUISITES.libxml_ext ? 'OK':'Por favor, habilite a extensão "libxml".')}}
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="cell">Extensão OpenSSL:</div>
                                 <div class="cell" ng-class="(PRE_REQUISITES.openssl_ext ? 'cell-success' : 'cell-error')">
-                                    {{(PRE_REQUISITES.openssl_ext ? 'OK':'Verifique as extensões carregadas.')}}
+                                    {{(PRE_REQUISITES.openssl_ext ? 'OK':'Por favor, habilite a extensão "openssl".')}}
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="cell">Extensão PCRE:</div>
                                 <div class="cell" ng-class="(PRE_REQUISITES.pcre_ext ? 'cell-success' : 'cell-error')">
-                                    {{(PRE_REQUISITES.pcre_ext ? 'OK':'Verifique as extensões carregadas.')}}
+                                    {{(PRE_REQUISITES.pcre_ext ? 'OK':'Por favor, habilite a extensão "pcre".')}}
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="cell">Extensão PDO:</div>
                                 <div class="cell" ng-class="(PRE_REQUISITES.PDO_ext ? 'cell-success' : 'cell-error')">
-                                    {{(PRE_REQUISITES.PDO_ext ? 'OK':'Verifique as extensões carregadas.')}}
+                                    {{(PRE_REQUISITES.PDO_ext ? 'OK':'Por favor, habilite a extensão "pdo".')}}
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="cell">Extensão PDO MySQL:</div>
                                 <div class="cell" ng-class="(PRE_REQUISITES.pdo_mysql_ext ? 'cell-success' : 'cell-error')">
-                                    {{(PRE_REQUISITES.pdo_mysql_ext ? 'OK':'Verifique as extensões carregadas.')}}
+                                    {{(PRE_REQUISITES.pdo_mysql_ext ? 'OK':'Por favor, habilite a extensão "pdo_mysql".')}}
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="cell">Extensão Sockets:</div>
                                 <div class="cell" ng-class="(PRE_REQUISITES.sockets_ext ? 'cell-success' : 'cell-error')">
-                                    {{(PRE_REQUISITES.sockets_ext ? 'OK':'Verifique as extensões carregadas.')}}
+                                    {{(PRE_REQUISITES.sockets_ext ? 'OK':'Por favor, habilite a extensão "sockets".')}}
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="cell">Extensão Zip:</div>
                                 <div class="cell" ng-class="(PRE_REQUISITES.zip_ext ? 'cell-success' : 'cell-error')">
-                                    {{(PRE_REQUISITES.zip_ext ? 'OK':'Verifique as extensões carregadas.')}}
+                                    {{(PRE_REQUISITES.zip_ext ? 'OK':'Por favor, habilite a extensão "zip".')}}
                                 </div>
                             </div>
                         </div>
@@ -284,21 +310,85 @@ $PRE_REQUISITES = array(
                         
                         <h1>Endereços</h1>
 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </div>
+                        <p>Segue abaixo as variáveis padrões de caminho para instalação do brACP.</p>
+
+                        <label data-info="URL de Instalação" data-warning="URL de acesso ao brACP. Caminho digitado no navegador para acessar o brACP.">
+                            <input type="text" ng-model="INSTALL_VARS.BRACP_URL"/>
+                        </label>
+
+                        <label data-info="Pasta de Instalação" data-warning="Pasta onde foi instalado o brACP. Necessário para configurar o .htaccess e também para requisições internas.">
+                            <input type="text" ng-model="INSTALL_VARS.BRACP_DIR_INSTALL_URL"/>
+                        </label>
+
+                        <label data-info="Caminho dos arquivos templates" data-warning="Caminho completo para onde os arquivos templates estão. Necessário para montar as telas do brACP.">
+                            <input type="text" ng-model="INSTALL_VARS.BRACP_TEMPLATE_DIR"/>
+                        </label>
+
+                        <label data-info="Caminho dos arquivos de entidade" data-warning="Caminho completo para onde os arquivos de banco de dados. Necessário para realizar os acessos a banco.">
+                            <input type="text" ng-model="INSTALL_VARS.BRACP_ENTITY_DIR"/>
+                        </label>
+
+                        <label data-info="Fuso Horário" data-warning="Fuso horário para gravar informações corretas de data e hora.">
+                            <select ng-model="INSTALL_VARS.BRACP_DEFAULT_TIMEZONE">
+                                <?php foreach(timezone_identifiers_list() as $timezone) { ?>
+                                    <option value="<?php echo $timezone; ?>"><?php echo $timezone; ?></option>
+                                <?php } ?>
+                            </select>
+                        </label>
+ 
+
+                   </div>
 
                     <div ng-if="STEP == 4" class="body">
                         
                         <h1>Expressões Regulares</h1>
 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <label data-info="Nome de usuário" data-warning="Expressão regular para campo de usuário, que tipo de caracteres serão aceitos.">
+                            <select ng-model="REGEXP_USERNAME">
+                                <option value="10">Somente letras e números.</option>
+                                <option value="20">Letras, números, espaços e caracteres especiais ( @ $ # % & * ! )</option>
+                                <option value="30">Campo livre</option>
+                            </select>
+                        </label>
+
+                        <label data-info="Senha de usuário" data-warning="Expressão regular para campo de senha, que tipo de caracteres serão aceitos.">
+                            <select ng-model="REGEXP_PASSWORD">
+                                <option value="01">Somente letras e números.</option>
+                                <option value="02">Letras, números, espaços e caracteres especiais ( @ $ # % & * ! )</option>
+                                <option value="03">Campo livre</option>
+                            </select>
+                        </label>
+
+                        <label data-info="E-mail de usuário" data-warning="Expressão regular para definir os e-mails de usuário.">
+                            <input type="text" ng-model="INSTALL_VARS.BRACP_REGEXP_EMAIL"/>
+                        </label>
                     </div>
 
                     <div ng-if="STEP == 5" class="body">
                         
                         <h1>reCAPTCHA</h1>
 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <input id="RECAPTCHA_ALLOW" ng-model="INSTALL_VARS.BRACP_RECAPTCHA_ENABLED" class="input-checkbox" type="checkbox">
+                        <label for="RECAPTCHA_ALLOW" class="input-checkbox" data-warning="Habilita proteção contra spams e flood de requisições.">Habilita o uso do reCAPTCHA para validar os usuários</label>
+
+                        <p ng-if="INSTALL_VARS.BRACP_RECAPTCHA_ENABLED" class="message info icon">
+                            A Validação reCAPTCHA somente irá aparecer após o usuário errar algumas vezes os formulários do sistema.<br>
+                            <br>
+                            Mais informações: <a href="https://www.google.com/recaptcha/intro/index.html" target="_blank">Google reCAPTCHA</a>
+                        </p>
+
+                        <label ng-if="INSTALL_VARS.BRACP_RECAPTCHA_ENABLED" data-info="Chave Privada" data-warning="Chave privada para o reCAPTCHA, usada para válidação com o servidor.">
+                            <input type="text" ng-model="INSTALL_VARS.BRACP_RECAPTCHA_PRIVATE_KEY"/>
+                        </label>
+
+                        <label ng-if="INSTALL_VARS.BRACP_RECAPTCHA_ENABLED" data-info="Chave Pública" data-warning="Chave pública para o reCAPTCHA, usada para gerar o código de validação.">
+                            <input type="text" ng-model="INSTALL_VARS.BRACP_RECAPTCHA_PRIVATE_KEY"/>
+                        </label>
+
+                        <label ng-if="INSTALL_VARS.BRACP_RECAPTCHA_ENABLED" data-info="URL para Verificação" data-warning="Endereço para validação dos códigos de retorno.">
+                            <input type="text" ng-model="INSTALL_VARS.BRACP_RECAPTCHA_PRIVATE_URL"/>
+                        </label>
+                        
                     </div>
 
                     <div ng-if="STEP == 6" class="body">
